d6ca148957c508d2046f3b7a349e844122b4e53b
Masud Alam <masud.alam.it2003@gmail.com>
2026-04-09 00:10:36 +0530

    polish the code

--- Student-Result-System/main.py
+++ Student-Result-System/main.py
@@ -1,72 +1,45 @@
-# from turtledemo.round_dance import stop
-# def student_score():
-#     student_name = input("Enter your Name:\n")
-#     physics = int(input("Enter your Physics marks:\n"))
-#     chemistry = int(input("Enter your Chemistry marks:\n"))
-#     math = int(input("Enter your math marks:\n"))
-#     sst = int(input("Enter your Social Science marks:\n"))
-#     english = int(input("Enter your English marks:\n"))
-#
-#     total_marks = physics + chemistry + math + sst + english
-#     average_marks = total_marks / 5
-#     input_data_in_list = [physics,chemistry,math,sst,english]
-#
-#     for i in input_data_in_list:
-#         if i < 33:
-#             print("You have less than 33 marks in your subject:\nFail ")
-#             break
-#         else:
-#             print(f"Name: {student_name}")
-#             print(f"Total marks: {total_marks}")
-#             print(f"Average marks: {average_marks}")
-#             break
-#
-#     if average_marks > 90:
-#         print("Grade A")
-#     elif 70 <= average_marks <= 89:
-#         print("Grade B")
-#     elif 50 <= average_marks <= 69:
-#         print("Grade C")
-#     else:
-#         print("Fail")
-#
-# student_score()
-#
-# more_score_check = input("You want to check other student score: Type 'Yes' for continue... or type No to stop!.").lower()
-# if more_score_check == "yes":
-#     student_score()
-# else:
-#     stop()
-#
 
+def student_score():
+    student_name = input("Enter your Name:\n")
 
+    physics = int(input("Enter your Physics marks:\n"))
+    chemistry = int(input("Enter your Chemistry marks:\n"))
+    math = int(input("Enter your math marks:\n"))
+    sst = int(input("Enter your Social Science marks:\n"))
+    english = int(input("Enter your English marks:\n"))
 
-student_name = input("Enter your Name:\n")
-physics = int(input("Enter your Physics marks:\n"))
-chemistry = int(input("Enter your Chemistry marks:\n"))
-math = int(input("Enter your math marks:\n"))
-sst = int(input("Enter your Social Science marks:\n"))
-english = int(input("Enter your English marks:\n"))
+    marks = [physics,chemistry,math,sst,english]
+    total_marks = sum(marks)
+    average_marks = total_marks / 5
 
-total_marks = physics + chemistry + math + sst + english
-average_marks = total_marks / 5
-marks = [physics,chemistry,math,sst,english]
-print(marks)
-is_fail = ""
-for score_num in marks:
-    if score_num < 33:
-        is_fail = "Fail"
-print(is_fail)
+    fail = False
+    for m in marks:
+        if m < 33:
+            fail = True
+            break
+    print("\n******* Result *********")
+    print(f"Name: {student_name}")
+    print(f"Total marks: {total_marks}")
+    print(f"Average marks: {average_marks}")
 
+    if fail:
+        print("Result: Fail(Less than 33 marks in a subject).")
+    else:
+        if average_marks > 90:
+            print("Grade A")
+        elif 70 <= average_marks <= 89:
+            print("Grade B")
+        elif 50 <= average_marks <= 69:
+            print("Grade C")
+        else:
+            print("Fail")
 
-# elif score_num > 33:
-# print(f"Total marks: {total_marks}")
-# print(f"Average marks: {average_marks}")
-# if average_marks > 90:
-#     print("Grade A")
-# elif 70 <= average_marks <= 89:
-#     print("Grade B")
-# elif 50 <= average_marks <= 69:
-#     print("Grade C")
-# else:
-#     print("Fail")
+while True:
+    student_score()
+
+    choice = input("\nDo you want to check another student? (yes/no): ").lower()
+
+    if choice != "yes":
+        print("Have a nice day!")
+        break
+
