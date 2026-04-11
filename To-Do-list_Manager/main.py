b8041cc0ddee4c9570567d01b076fea157aca64b
Masud Alam <masud.alam.it2003@gmail.com>
2026-04-12 01:23:34 +0530

    Complete to do list manager

--- To-Do-list_Manager/main.py
+++ To-Do-list_Manager/main.py
@@ -1,26 +1,73 @@
-
 todo_list = []
-print("------- To-D0 List Creator -------")
-print("Add your task one by one: Type 'exit' to finish.\n")
+
+print("\nTo-Do List Manager!")
+print("\nPress 1 to Add Task.")
+print("Press 2 to View Task.")
+print("Press 3 to Remove task.")
+print("Press 4 to Exit.\n")
+
 while True:
-    add_task = input("Enter your To-Do List: ").strip()  # .strip will remove the accidental space of both the side
-    if add_task.lower() == "exit":
+
+    choice = input("Enter your choice: ").strip()
+
+    # ADD
+    if choice == "1":
+        while True:
+            task = input("Enter task (type 'exit' to stop adding): ").strip()
+
+            if task.lower() == "exit":
+                break
+            elif task:
+                todo_list.append(task)
+                print("Task added!")
+            else:
+                print("Task cannot be empty.")
+
+    # VIEW
+    elif choice == "2":
+        if not todo_list:
+            print("Your list is empty.")
+        else:
+            print("\nYour Tasks:")
+            for idx, task in enumerate(todo_list, start=1):
+                print(f"{idx}. {task}")
+
+    # REMOVE
+    elif choice == "3":
+        if not todo_list:
+            print("Nothing to remove.")
+        else:
+            while True:
+                print("\nYour Tasks:")
+                for idx, task in enumerate(todo_list, start=1):
+                    print(f"{idx}. {task}")
+
+                remove = input("Enter task number to remove (or 'exit'): ")
+
+                if remove.lower() == "exit":
+                    break
+
+                if not todo_list:
+                    break
+
+                if remove.isdigit():
+                    index = int(remove) - 1
+
+                    if 0 <= index < len(todo_list):
+                        removed = todo_list.pop(index)
+                        print(f"Removed: {removed}")
+                    else:
+                        print("Invalid number.")
+                else:
+                    print("Enter a valid number.")
+
+    # EXIT
+    elif choice == "4":
+        print("Program ended.")
         break
-    else:
-        todo_list.append(add_task)
-
-    # To show Task
-    for idx,task in enumerate(todo_list,start=1):
-        view_task = f"{idx}.{task}"
-        print(view_task)
-
-    remove_task = input("Enter Number to remove Task: ")
-    if remove_task.isdigit():
-        if 1 <= remove_task <= len(todo_list):
-            print(todo_list.pop(remove_task - 1))
 
     else:
-        print("Invalid input.")
+        print("Invalid choice. Try again.")
 
 
 
