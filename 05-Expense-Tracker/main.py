ab3847891cac64841555321b16d8b451b4c9077a
Masud Alam <masud.alam.it2003@gmail.com>
2026-04-13 23:57:55 +0530

    added show expenses

--- 05-Expense-Tracker/main.py
+++ 05-Expense-Tracker/main.py
@@ -1,30 +1,43 @@
 # Add Expense
-from unicodedata import category
 
 print("Welcome to Expense Tracker Website. Add your today Expenses.")
 print("Enter 1 👉 add expenses. 2 👉 show expense. 3 👉 See total expenses.")
 print("---------------------------------------------------------------\n")
 
-choice = input("Enter number: ")
 expenses = []
 
-# Add Expenses
-if choice == "1":
+while True:
+    choice = input("Enter number: ").strip()
     categories = ["Food", "Travel", "Shopping", "Entra"]
-    for cat in categories:
-        while True:
-            amount = input(f"Enter Your {cat} Expenses in $: ")
-            if amount.isdigit():
-                expenses.append(amount)
-                break
-            else:
-                print("Invalid Input. Please enter amount in digit.")
+    # Add Expenses
+    if choice == "1":
+        for cat in categories:
+            while True:
+                amount = input(f"Enter Your {cat} Expenses in $: ").strip()
+                if amount.isdigit():
+                    expenses.append(amount)
+                    break
+                else:
+                    print("Invalid Input. Please enter amount in digit.")
 
-else:
-    print("Invalid Input!")
+    # Show the Expenses.
+    elif choice == "2":
+        if not expenses:
+            print("Your list is Empty:")
+        else:
+            print("Your Expenses by Category.")
+            for idx, individual_category in enumerate(categories and expenses, start=1):
+                print(f"{idx}.{individual_category}: ")
 
-print(expenses)
-# Show Expenses in list with numbering
+            print(expenses)
+
+    elif choice == "4":
+        break
+    else:
+        print("Invalid Input!")
+        break
+
+
 
 
 
